ae9e7bf88755dae2a22a51a9426f2be6f696059e
Rocky Madden <git@rockymadden.com>
2014-06-03 03:34:40 -0600

    Code formatting tweak.

--- droplr.sh
+++ droplr.sh
@@ -4,5 +4,10 @@ abspath() {
   [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-osascript -e 'tell application "Droplr"' -e "set filePath to \"$(abspath "$1")\"" -e 'set fileContents to (do shell script "cat " & quoted form of (POSIX path of filePath))' -e 'set the clipboard to fileContents' -e 'end tell'
+osascript -e 'tell application "Droplr"' \
+  -e "set filePath to \"$(abspath "$1")\"" \
+  -e 'set fileContents to (do shell script "cat " & quoted form of (POSIX path of filePath))' \
+  -e 'set the clipboard to fileContents' \
+  -e 'end tell'
+
 osascript -e 'tell application "System Events" to keystroke "d" using option down'

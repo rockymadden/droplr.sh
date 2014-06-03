2979bb4c2488bd69a64e05b109b85323e78aa99a
Rocky Madden <git@rockymadden.com>
2014-06-03 03:30:45 -0600

    Created repository.

---
+++ droplr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+abspath() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+osascript -e 'tell application "Droplr"' -e "set filePath to \"$(abspath "$1")\"" -e 'set fileContents to (do shell script "cat " & quoted form of (POSIX path of filePath))' -e 'set the clipboard to fileContents' -e 'end tell'
+osascript -e 'tell application "System Events" to keystroke "d" using option down'
